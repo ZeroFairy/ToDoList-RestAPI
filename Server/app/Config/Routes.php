04f65bccd5288738544383d0cd3b663f3ce33873
ZeroFairy <jordanjv453@gmail.com>
2024-10-27 22:37:09 +0700

    added the middleware

--- Server/app/Config/Routes.php
+++ Server/app/Config/Routes.php
@@ -7,18 +7,19 @@ use CodeIgniter\Router\RouteCollection;
  */
 $routes->get('/', 'Home::index');
 
-$routes->get('/server/user', 'UserController::index');
-$routes->post('/server/user', 'UserController::signup');
+// $routes->get('/server/user', 'UserController::index');
+// $routes->post('/server/user', 'UserController::signup'); //create
+// $routes->post('/server/user/(:num)', 'UserController::show/$1');
+// $routes->delete('/server/user/(:num)', 'UserController::delete/$1');
+// $routes->put('/server/user/(:num)', 'UserController::update/$1');
+// $routes->post('/server/user/update/(:num)', 'UserController::update/$1');
+
 $routes->post('/server/login', 'UserController::login');
-$routes->post('/server/user/(:num)', 'UserController::show/$1');
-$routes->delete('/server/user/(:num)', 'UserController::delete/$1');
-$routes->put('/server/user/(:num)', 'UserController::update/$1');
-$routes->post('/server/user/update/(:num)', 'UserController::update/$1');
 
 // Protect task routes with the 'auth' middleware
-$routes->group('tasks', ['filter' => 'auth'], function($routes) {
-    $routes->get('/', 'TaskController::index');     // Protected route
-    $routes->post('/', 'TaskController::create');   // Protected route
-    $routes->put('(:segment)', 'TaskController::update/$1');  // Protected route
-    $routes->delete('(:segment)', 'TaskController::delete/$1');  // Protected route
+$routes->group('server/user', ['filter' => 'auth'], function($routes) {
+    $routes->get('/', 'UserController::index');     // Protected route
+    $routes->post('/', 'UserController::signup');   // Protected route
+    $routes->put('(:num)', 'UserController::update/$1');  // Protected route
+    $routes->delete('(:num)', 'UserController::delete/$1');  // Protected route
 });
