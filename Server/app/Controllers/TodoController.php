0b07bf9c9083b29eff426a744c0310adc5c9ecfd
ZeroFairy <jordanjv453@gmail.com>
2024-10-30 11:23:42 +0700

    init commit

---
+++ Server/app/Controllers/TodoController.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Controllers;
+
+use CodeIgniter\HTTP\ResponseInterface;
+use CodeIgniter\RESTful\ResourceController;
+
+class TodoController extends ResourceController
+{
+    protected $modelName = 'App\Models\ToDo';
+    protected $format = 'json';
+    /**
+     * Return an array of resource objects, themselves in array format.
+     *
+     * @return ResponseInterface
+     */
+    public function index()
+    {
+        $data = [
+            'message' => 'success',
+            'data_tasks' => $this->model->orderBy('id', 'ASC')->findAll()
+        ];
+
+        return $this->respond($data, 200);
+    }
+
+    /**
+     * Return the properties of a resource object.
+     *
+     * @param int|string|null $id
+     *
+     * @return ResponseInterface
+     */
+    public function show($id = null)
+    {
+        
+    }
+
+    /**
+     * Return a new resource object, with default properties.
+     *
+     * @return ResponseInterface
+     */
+    public function new()
+    {
+        //
+    }
+
+    /**
+     * Create a new resource object, from "posted" parameters.
+     *
+     * @return ResponseInterface
+     */
+    public function create()
+    {
+        $rules = $this->validate([
+            'task' => 'required'
+        ]);
+
+        if (!$rules) {
+            $reponse = [
+                'message' => $this->validator->getErrors()
+            ];
+
+            return $this->failValidator($reponse);
+        }
+
+        $id_user = 
+
+        $this->model->insert([
+            'task' => esc($this->request->getVar('task')),
+            'id_user' => $id_user
+        ]);
+    }
+
+    /**
+     * Return the editable properties of a resource object.
+     *
+     * @param int|string|null $id
+     *
+     * @return ResponseInterface
+     */
+    public function edit($id = null)
+    {
+        //
+    }
+
+    /**
+     * Add or update a model resource, from "posted" properties.
+     *
+     * @param int|string|null $id
+     *
+     * @return ResponseInterface
+     */
+    public function update($id = null)
+    {
+        //
+    }
+
+    /**
+     * Delete the designated resource object from the model.
+     *
+     * @param int|string|null $id
+     *
+     * @return ResponseInterface
+     */
+    public function delete($id = null)
+    {
+        //
+    }
+}
