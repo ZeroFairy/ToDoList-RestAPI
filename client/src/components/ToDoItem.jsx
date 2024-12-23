97b3bf076c33ab1696cf80110b0e34fe259932f2
ZeroFairy <jordanjv453@gmail.com>
2024-12-23 15:43:10 +0700

    change a little bit, cause backend have handle it

--- client/src/components/ToDoItem.jsx
+++ client/src/components/ToDoItem.jsx
@@ -1,11 +1,11 @@
-export function ToDoItem ({ completed, id, title, toggleTodo, deleteTodo }) {
+export function ToDoItem({ completed, id, title, toggleTodo, deleteTodo }) {
     return (
         <li>
             <label>
                 <input 
                     type="checkbox" 
                     checked={completed}
-                    onChange={e => toggleTodo(id, e.target.checked)} 
+                    onChange={() => toggleTodo(id)} 
                 />
                 {title}
             </label>
